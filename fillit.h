d67762a5226484d8f898aff471dbdec6d1311bfb
Eric LI <erli@student.42.fr>
2018-11-15 15:03:26 +0100

    Correction imminente

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: gly <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 13:30:01 by gly               #+#    #+#             */
-/*   Updated: 2018/11/15 15:01:33 by erli             ###   ########.fr       */
+/*   Updated: 2018/11/15 15:03:12 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@
 # include <stdlib.h>
 # include <fcntl.h>
 
-typedef	struct		s_position
+typedef	struct		s_pos
 {
 	int				row;
 	int				col;
-}					t_position;
+}					t_pos;
 
 typedef struct		s_piece
 {
 	int				col;
 	int				row;
 	char			**form;
-	t_position		*pos;
+	t_pos			*pos;
 	struct s_piece	*next;
 	struct s_piece	*prev;
 }					t_piece;
