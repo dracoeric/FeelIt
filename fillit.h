5ab6679dbd78c721d52545880218041f6e44172d
Eric LI <erli@student.42.fr>
2018-11-15 14:45:01 +0100

    Changement de fillit

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: gly <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 13:30:01 by gly               #+#    #+#             */
-/*   Updated: 2018/11/15 13:48:39 by gly              ###   ########.fr       */
+/*   Updated: 2018/11/15 14:43:17 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FILLIT_H
 
 # include <stdlib.h>
+# include <fcntl.h>
 
 typedef struct		s_piece
 {
@@ -29,5 +30,8 @@ typedef struct		s_board
 	int				size;
 	unsigned char	**mat;
 }					t_board;
+
+int		bigger_board(t_board *board, int nb_piece);
+void	free_mat(char **mat, int n_row);
 
 #endif
